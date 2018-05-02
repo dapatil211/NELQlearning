7b5f4d9619a179d977534d3627f52db057472c57
Darshan Patil <dapatil211@gmail.com>
2018-05-01 20:55:47 -0400

    Added fast decay after trigger to linear epsilon policy

--- epsilon_policy.py
+++ epsilon_policy.py
@@ -14,20 +14,28 @@ class EpsilonPolicy(with_metaclass(abc.ABCMeta, object)):
 
 
 class LinearlyDecayingEpsilonPolicy(EpsilonPolicy):
-    def __init__(self, start_step, start_value, difference, lower_bound=0.0):
+    def __init__(
+            self, start_step, start_value, difference, trigger_difference=.0001,
+            trigger_epsilon=.8, lower_bound=0.0):
         self.start_step = start_step
         self.start_value = start_value
         self.difference = difference
+        self.trigger_difference = trigger_difference
+        self.trigger_epsilon = trigger_epsilon
         self.lower_bound = lower_bound
         self._current_step = 0
 
     def get_epsilon(self, current_epsilon, triggered=False):
         self._current_step += 1
-        if self._current_step > self.start_step:
-            value = current_epsilon - self.difference
+            
+        if self._current_step < self.start_step:
+            return self.start_value
+        elif triggered:
+            self.difference = self.trigger_difference
+            return self.trigger_epsilon
         else:
-            value = self.start_value
-        return max(value, self.lower_bound)
+            value = current_epsilon - self.difference
+            return max(value, self.lower_bound)
 
 
 class ExponentiallyDecayingEpsilonPolicy(EpsilonPolicy):
